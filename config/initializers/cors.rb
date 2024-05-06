34ace7a27274f463719aa5b44beb0adf6fa3a7a8
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-05-05 20:55:58 -0700

    cors pluralization

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173","https://mini-capstone-frontend-d5sd.onrender.com/"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    origins "example.com", "localhost:5173"
+    resources "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
